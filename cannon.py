d948aed5a47b15f683d37b07e887e743647dc16b
Alex <a01828674@tec.mx>
2026-09-17 08:49:15 -0600

    Se aumenta la velocidad del proyectil y balon

--- cannon.py
+++ cannon.py
@@ -1,36 +1,30 @@
 """Cannon, hitting targets with projectiles.
-
 Exercises
-
 1. Keep score by counting target hits.
 2. Vary the effect of gravity.
 3. Apply gravity to the targets.
 4. Change the speed of the ball.
 """
-
 from random import randrange
 from turtle import *
-
 from freegames import vector
 
 ball = vector(-200, -200)
 speed = vector(0, 0)
 targets = []
 
-
 def tap(x, y):
     """Respond to screen tap."""
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 25
-        speed.y = (y + 200) / 25
-
+        # Velocidad del proyectil aumentada (divisor cambiado de 25 a 10)
+        speed.x = (x + 200) / 10
+        speed.y = (y + 200) / 10
 
 def inside(xy):
     """Return True if xy within screen."""
     return -200 < xy.x < 200 and -200 < xy.y < 200
-
 
 def draw():
     """Draw ball and targets."""
@@ -46,7 +40,6 @@ def draw():
 
     update()
 
-
 def move():
     """Move ball and targets."""
     if randrange(40) == 0:
@@ -55,7 +48,8 @@ def move():
         targets.append(target)
 
     for target in targets:
-        target.x -= 0.5
+        # Velocidad de los balones aumentada (de 0.5 a 1.5)
+        target.x -= 1.5
 
     if inside(ball):
         speed.y -= 0.35
@@ -75,7 +69,6 @@ def move():
             return
 
     ontimer(move, 50)
-
 
 setup(420, 420, 370, 0)
 hideturtle()
